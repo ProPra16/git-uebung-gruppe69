cc8112edc5a5b4fe4314870ed2caa7d18a269d6d
Anne <anne.abraham@hhu.de>
2016-06-16 11:32:25 +0200

    Button C - Exit hinzugefügt

--- src/de/hhu/propra16/FiveButtons.java
+++ src/de/hhu/propra16/FiveButtons.java
@@ -1,9 +1,11 @@
 package de.hhu.propra16;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 /**
@@ -19,6 +21,8 @@ public class FiveButtons extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/five_buttons" +
                 ".fxml"));
 
+        
+       
         primaryStage.setTitle("Five Buttons");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,7 +1,13 @@
 package de.hhu.propra16;
 
+import javafx.application.Platform;
+
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
+
+    public void exit() {
+       Platform.exit();
+    }
 }
