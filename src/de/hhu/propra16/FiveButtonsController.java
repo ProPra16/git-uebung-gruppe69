c95b08483ccc3cca27b0bedecc7bcf8fd21b46b9
Felix Michels <felixm.michels@gmail.com>
2016-06-16 11:26:12 +0200

    Funktion geschrieben, die Datum anzeigt und mit dem Button verbunden

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,7 +1,19 @@
 package de.hhu.propra16;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.time.LocalDate;
+
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
+
+    @FXML
+    Label dateLabel;
+
+    public void showDate() {
+        dateLabel.setText(LocalDate.now().toString());
+    }
 }
