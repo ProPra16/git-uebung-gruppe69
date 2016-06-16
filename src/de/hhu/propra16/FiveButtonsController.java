bd6cb18fd6c077677aad2fb8ab56c31a007132d6
Anne <anne.abraham@hhu.de>
2016-06-16 11:54:56 +0200

    button alert

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -2,6 +2,7 @@ package de.hhu.propra16;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import java.time.LocalDate;
 
@@ -20,4 +21,14 @@ public class FiveButtonsController {
     public void showDate() {
         dateLabel.setText(LocalDate.now().toString());
     }
+
+    public void alert(){
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Help");
+            alert.setHeaderText(null);
+            alert.setContentText("too many buttons");
+            alert.showAndWait();
+
+        }
 }
